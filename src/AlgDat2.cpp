e8def07c5dcebedcf225e1c7d6732deadc8aa5e9
unknown <alexd@fs.cs.hm.edu>
2018-12-06 14:29:53 +0100

    O haha reingeschaut

--- src/AlgDat2.cpp
+++ src/AlgDat2.cpp
@@ -7,7 +7,9 @@
 //============================================================================
 
 #include <iostream>
-#include <tuple>
+#include <algorithm>
+#include <string>
+#include <sstream>
 #include "Trie.h"
 using namespace std;
 typedef pair<string, string> wordpair;
@@ -28,13 +30,79 @@ void preFill(){
 
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "your a cunt. So lets start" << endl;
+
+	//Stuff I need in the Main
+
 	Trie<string, string> tree;
-
 	bool isDone = false;
+	string input;
 
+
+	//------------------------
+
+
+	cout << "Welcome to the World trie?" << endl;
+	cout << "	  /\_/\  (" << endl;
+	cout << "	 ( ^.^ ) _)" << endl;
+	cout << "	   \"/  (" << endl;
+	cout << "	 ( | | )" << endl;
+	cout << "	(__d b__)" << endl;
+
+	cout << "To fill in a set of word type: fill" << endl;
+	cout << "To enter a word pair do as following:" << endl;
 	cout << "Example: welt world" << endl;
+	cout << "Use the command show to have a visual of the trie" << endl;
+
+
+
+	while(!isDone){
+
+		std::getline(cin,input);
+		input = std::transform(input.begin(), input.end(),input.begin(), ::tolower);
+		// muss das verhalten von kuhaku pruefen wegen lowercase
+
+		switch(input){
+
+		case "fill":
+			preFill();
+			break;
+
+		case "show":
+			tree.toString();
+			break;
+
+		case "end" :
+			isDone = true;
+			tree.toString();
+			break;
+
+		default:
+			string word;
+			string trans;
+			stringstream stream;
+			stream.str(input);
+			stream >> word >> trans;
+
+
+			tree.insert(wordpair(word,trans));
+
+			stream.clear();
+			word.clear();
+			trans.clear();
+			cin.clear();
+
+			break;
+
+		}
+
+
+	}
+
+
+
+
+
+
 	while(!isDone){
 		cout << "Please enter word pairs." << endl;
 		string word;
